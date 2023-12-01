765545089695e996af266ac22b5246e6e1d29d45
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2023-12-01 00:01:53 -0500

    First Graphql Project

---
+++ src/test/java/com/accounts/gql/SpringBootGrapghQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounts.gql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGrapghQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
