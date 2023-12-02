0694ea38c94000900c2d9edc6fd1db29f333acad
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2023-12-02 01:47:18 -0500

    GrapghQL in Action

--- src/main/java/com/accounts/controller/AccountsController.java
+++ src/main/java/com/accounts/controller/AccountsController.java
@@ -29,7 +29,6 @@ public class AccountsController {
 
     @SchemaMapping (typeName = "BankAccount", field = "client")
     Client getClient (BankAccount account) {
-        log.info("Getting client for " + account.getId());
         return clientService.getClientByAccountId(account.getId());
     }
 }
