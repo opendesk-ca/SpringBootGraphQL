8950fc28bac645fbcadb4b785716cb4cfb52f5e0
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2023-12-02 22:16:15 -0500

    Update  Services

--- src/main/java/com/accounts/service/BankService.java
+++ src/main/java/com/accounts/service/BankService.java
@@ -12,14 +12,14 @@ import java.util.List;
 @Service
 @Slf4j
 public class BankService {
-    public List<BankAccount> getAccounts() {
 
-        List<BankAccount> bankAccounts = Arrays.asList(
-                new BankAccount("A100", "C100", Currency.USD, 106.00f, "A"),
-                new BankAccount("A101", "C200", Currency.CAD, 250.00f, "A"),
-                new BankAccount("A102", "C300", Currency.CAD, 333.00f, "I"),
-                new BankAccount("A103", "C400", Currency.EUR, 4000.00f, "A")
-        );
+    private static List<BankAccount> bankAccounts = Arrays.asList(
+            new BankAccount("A100", "C100", Currency.USD, 106.00f, "A"),
+            new BankAccount("A101", "C200", Currency.CAD, 250.00f, "A"),
+            new BankAccount("A102", "C300", Currency.CAD, 333.00f, "I"),
+            new BankAccount("A103", "C400", Currency.EUR, 4000.00f, "A")
+    );
+    public List<BankAccount> getAccounts() {
         return bankAccounts;
     }
 }
--- src/main/java/com/accounts/service/ClientService.java
+++ src/main/java/com/accounts/service/ClientService.java
@@ -1,8 +1,6 @@
 package com.accounts.service;
 
-import com.accounts.domain.BankAccount;
 import com.accounts.domain.Client;
-import com.accounts.domain.Currency;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +11,13 @@ import java.util.List;
 @Slf4j
 public class ClientService {
 
-    public Client getClientByAccountId (String accountId) {
-        log.info("Getting getClientByAccountId : " + accountId);
-        List <Client> clients = Arrays.asList(
-                new Client ("C100", "A100", "aaaa", "bbb", "cccc"),
-                new Client ("C200", "A102", "1111", "222", "333"),
-                new Client ("C300", "A102", "ppp", "qqq", "rrrr")
-        );
+    private static List <Client> clients = Arrays.asList(
+            new Client ("C100", "A100", "aaaa", "bbb", "cccc"),
+            new Client ("C200", "A102", "1111", "222", "333"),
+            new Client ("C300", "A102", "ppp", "qqq", "rrrr")
+    );
 
+    public Client getClientByAccountId (String accountId) {
         return clients.stream().filter(c->c.getAccountId().equals(accountId)).findFirst().orElse(null);
     }
 }
