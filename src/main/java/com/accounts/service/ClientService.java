45d6d79ec480c512797a97c79205fa87c3cebcfe
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2023-12-03 00:18:38 -0500

    Schema Changes

--- src/main/java/com/accounts/service/ClientService.java
+++ src/main/java/com/accounts/service/ClientService.java
@@ -7,20 +7,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 @Slf4j
 public class ClientService {
 
+    private List <Client> clients = Collections.emptyList();
+
     public Client getClientByAccountId (String accountId) {
         log.info("Getting getClientByAccountId : " + accountId);
-        List <Client> clients = Arrays.asList(
-                new Client ("C100", "A100", "aaaa", "bbb", "cccc"),
-                new Client ("C200", "A102", "1111", "222", "333"),
-                new Client ("C300", "A102", "ppp", "qqq", "rrrr")
-        );
-
         return clients.stream().filter(c->c.getAccountId().equals(accountId)).findFirst().orElse(null);
     }
 }
