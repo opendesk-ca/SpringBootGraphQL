f4151cb45e7597d7f633016fbb73ee41384fca16
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2023-12-09 12:38:23 -0500

    Service restructured.

--- src/main/java/com/accounts/service/BankService.java
+++ src/main/java/com/accounts/service/BankService.java
@@ -14,15 +14,19 @@ import java.util.List;
 @Slf4j
 public class BankService {
     private static List<BankAccount> bankAccounts = Arrays.asList(
-            new BankAccount("A100", "C100", Currency.USD, 106.00f, "A"),
-            new BankAccount("A101", "C200", Currency.CAD, 250.00f, "A"),
-            new BankAccount("A102", "C300", Currency.CAD, 333.00f, "I"),
-            new BankAccount("A103", "C400", Currency.EUR, 4000.00f, "A")
+            new BankAccount("A100", "C100", Currency.USD, 1500.00f, "Active"),
+            new BankAccount("A101", "C101", Currency.CAD, 3000.00f, "Active"),
+            new BankAccount("A102", "C102", Currency.EUR, 2500.00f, "Inactive"),
+            new BankAccount("A103", "C103", Currency.USD, 5000.00f, "Active"),
+            new BankAccount("A104", "C104", Currency.EUR, 7500.00f, "Active")
     );
-    private static List <Client> clients = Arrays.asList(
-            new Client ("C100", "A100", "aaaa", "bbb", "cccc"),
-            new Client ("C200", "A102", "1111", "222", "333"),
-            new Client ("C300", "A102", "ppp", "qqq", "rrrr")
+
+    private static List<Client> clients = Arrays.asList(
+            new Client("C100", "A100", "John", "T.", "Doe"),
+            new Client("C101", "A101", "Emma", "B.", "Smith"),
+            new Client("C102", "A102", "James", "R.", "Brown"),
+            new Client("C103", "A103", "Olivia", "S.", "Johnson"),
+            new Client("C104", "A104", "William", "K.", "Jones")
     );
 
     public List<BankAccount> getAccounts() {
