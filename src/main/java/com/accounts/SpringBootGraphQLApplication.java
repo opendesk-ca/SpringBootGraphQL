899bf27042edc4751fc45ba147640e79478d425f
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2023-12-23 12:25:47 -0500

    Added test Data

--- src/main/java/com/accounts/SpringBootGraphQLApplication.java
+++ src/main/java/com/accounts/SpringBootGraphQLApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootGrapghQlApplication {
+public class SpringBootGraphQLApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGrapghQlApplication.class, args);
+		SpringApplication.run(SpringBootGraphQLApplication.class, args);
 	}
 
 }
