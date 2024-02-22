f2be3c3b9d0d5751a7099b480392da374f9226aa
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-02-22 00:29:26 -0500

    Add Customised error messages.

--- src/main/java/com/accounts/exceptions/RequestErrorInterceptor.java
+++ src/main/java/com/accounts/exceptions/RequestErrorInterceptor.java
@@ -2,35 +2,62 @@ package com.accounts.exceptions;
 
 import graphql.GraphQLError;
 import graphql.GraphqlErrorBuilder;
+import org.springframework.graphql.ResponseError;
+import org.springframework.graphql.execution.ErrorType;
 import org.springframework.graphql.server.WebGraphQlInterceptor;
 import org.springframework.graphql.server.WebGraphQlRequest;
 import org.springframework.graphql.server.WebGraphQlResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class RequestErrorInterceptor  implements WebGraphQlInterceptor {
 
     @Override
     public Mono<WebGraphQlResponse> intercept(WebGraphQlRequest request, Chain chain) {
-        return chain.next(request).map(response -> {
-            if (response.isValid()) {
-                return response;
-            }
-
-            List<GraphQLError> errors = response.getErrors().stream()
-                    .map(error -> {
-                        GraphqlErrorBuilder<?> builder = GraphqlErrorBuilder.newError().message("Validation Error");
-                        // ...
-                        return builder.build();
-                    })
-                    .toList();
-
-            return response.transform(builder -> builder.errors(errors).build());
-        });
+        return chain.next(request)
+                .map(this::processResponse);
     }
 
+    private WebGraphQlResponse processResponse(WebGraphQlResponse response) {
+        if (response.isValid()) {
+            return response;
+        } else {
+            /** There are errors in the response - so, lets handle it*/
+            List<GraphQLError> modifiedErrors = modifyErrors(response.getErrors());
+            return response.transform(builder -> builder.errors(modifiedErrors).build());
+        }
+    }
 
+    private List<GraphQLError> modifyErrors(List<ResponseError> originalErrors) {
+        return originalErrors.stream()
+                .map(this::createValidationError)
+                .collect(Collectors.toList());
+    }
+
+    private GraphQLError createValidationError(ResponseError error) {
+        String errorMessage = null;
+        Map<String, Object> extensionMap = new HashMap<>();
+
+        if (error.getMessage().contains("is not a valid 'CountryCode'")){
+            errorMessage = "The Country code entered is not valid. Please provide a valid country code";
+            extensionMap.put("Country Code", "Currently we only support US and Canada");
+        }else if (error.getMessage().contains("currency")){
+            errorMessage = "Please provide a valid Currency code";
+            extensionMap.put("Currency Code", "Currently we only support USD and CAD");
+        }
+
+        return GraphqlErrorBuilder.newError()
+        .message(errorMessage)
+        .errorType(ErrorType.BAD_REQUEST)
+                .extensions(extensionMap)
+                .locations(error.getLocations())
+        // Add more customization to the error as needed
+        .build();
+    }
 }
