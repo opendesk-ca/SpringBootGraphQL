20472271051d6b17ce6fdda5db48debfa95346fd
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-09-28 11:18:00 -0400

    Added missing class - for oAuth Authentication

--- src/main/java/com/accounts/domain/BankAccount.java
+++ src/main/java/com/accounts/domain/BankAccount.java
@@ -1,17 +1,3 @@
 package com.accounts.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import java.util.Arrays;
-import java.util.List;
-
-@AllArgsConstructor
-@Data
-public class BankAccount {
-    private  String id;
-    private String clientId;
-    private Currency currency;
-    private Float balance;
-    private String status;
-}
+public record BankAccount(String id, String clientId, Currency currency, float balance, String status) {}
